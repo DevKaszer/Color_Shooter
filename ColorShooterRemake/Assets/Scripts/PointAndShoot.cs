68d4c5b300407c36417cb29bd5c20f28d1acc7cb
Kaszer <88002228+DevKaszer@users.noreply.github.com>
2022-02-16 23:04:12 +0100

    001.1
    corection

--- ColorShooterRemake/Assets/Scripts/PointAndShoot.cs
+++ ColorShooterRemake/Assets/Scripts/PointAndShoot.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class PointAndShoot : MonoBehaviour
 {
-    private Camera _cam;
-            
-        
-
     // Start is called before the first frame update
     void Awake()
     {
-        _cam = Camera.main;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        var mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
 
-        transform.up = mousePos;
     }
 }
