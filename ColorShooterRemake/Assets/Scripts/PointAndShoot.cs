f3259d82d74b1288d69ec87e75168cdb14bf9cd2
Kaszer <88002228+DevKaszer@users.noreply.github.com>
2022-02-17 11:42:40 +0100

    002 Aiming

--- ColorShooterRemake/Assets/Scripts/PointAndShoot.cs
+++ ColorShooterRemake/Assets/Scripts/PointAndShoot.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PointAndShoot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Awake()
-    {
+    public GameObject crosshairs;
+    public GameObject player;
 
+    private Vector3 target;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        crosshairs.transform.position = new Vector2(target.x, target.y);
 
+        Vector3 difference = target - player.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);    
     }
 }
