43f42dcc433b7a9bed2f6166aa17c7adeb04a729
Kaszer <88002228+DevKaszer@users.noreply.github.com>
2022-02-18 20:04:06 +0100

    003 Shooting

--- ColorShooterRemake/Assets/Scripts/PointAndShoot.cs
+++ ColorShooterRemake/Assets/Scripts/PointAndShoot.cs
@@ -6,7 +6,10 @@ public class PointAndShoot : MonoBehaviour
 {
     public GameObject crosshairs;
     public GameObject player;
+    private Shooting shooting;
 
+    public Transform FirePoint2;
+        
     private Vector3 target;
 
     // Use this for initialization
@@ -18,11 +21,20 @@ public class PointAndShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MouseCamera();
+        player.transform.rotation = FirePoint2.transform.rotation;
+    }
+
+
+    public void MouseCamera()
+    {
+        // Cursor
         target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
         crosshairs.transform.position = new Vector2(target.x, target.y);
 
+        // Player
         Vector3 difference = target - player.transform.position;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);    
+        player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
     }
 }
---
+++ ColorShooterRemake/Assets/Scripts/Shooting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject PrefabAmmo;
+    public Transform Firepoint;
+    public float SpeedAmmo = 1f;
+    private Rigidbody2D rb;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        var NewBullet = Instantiate(PrefabAmmo.transform, Firepoint.transform.position, Firepoint.transform.rotation);
+        rb = NewBullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(Firepoint.right * SpeedAmmo, ForceMode2D.Impulse);
+    }
+
+}
